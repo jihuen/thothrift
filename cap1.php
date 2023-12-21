cf6536e20170afba196a75c98b14b356e8c82712
waversint <134621430+waversint@users.noreply.github.com>
2023-12-22 02:07:29 +0800

    Update and rename running1.php to cap1.php

--- cap1.php
+++ cap1.php
@@ -108,11 +108,11 @@
 
 		<div class="nav1">
 			<ul>
-				<li><a href="product1.php">Basketball</a></li>
+				<li><a href="product1.php">Top</a></li>
 				<li>|</li>
-				<li><a href="football1.php">Football</a></li>
+				<li><a href="bottom1.php">Bottom</a></li>
 				<li>|</li>
-				<li><a href="running1.php" class="active" style="color:#111;">Running</a></li>
+				<li><a href="cap1.php" class="active" style="color:#111;">Cap</a></li>
 			</ul>
 				<a href="cart.php"><button class="btn btn-inverse" style="right:1%; position:fixed; top:10%;"><i class="icon-shopping-cart icon-white"></i> View Cart</button></a>
 		</div>
@@ -140,7 +140,7 @@
 						}else{
 							echo "<div class='float'>";
 							echo "<center>";
-							echo "<a href='details.php?id=".$fetch['product_id']."'><img class='img-polaroid' src='photo/".$fetch['product_image']."' height = '300px' width = '300px'></a>";
+							echo "<a href='details.php?id=".$fetch['product_id']."'><img class='img-polaroid' src='picture/".$fetch['product_image']."' height = '300px' width = '300px'></a>";
 							echo "".$fetch['product_name']."";
 							echo "<br />";
 							echo "P ".$fetch['product_price']."";
