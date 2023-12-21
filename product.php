3e3c151290df37971c03febe7dd764c755118288
waversint <134621430+waversint@users.noreply.github.com>
2023-12-22 02:19:11 +0800

    Update product.php
    tukar part gambar dan video

--- product.php
+++ product.php
@@ -114,11 +114,11 @@
 
 	<div class="nav1">
 		<ul>
-			<li><a href="product.php" class="active" style="color:#111;">Basketball</a></li>
+			<li><a href="product.php" class="active" style="color:#111;">Top</a></li>
 			<li>|</li>
-			<li><a href="football.php">Football</a></li>
+			<li><a href="bottom.php">Bottom</a></li>
 			<li>|</li>
-			<li><a href="running.php">Running</a></li>
+			<li><a href="cap.php">Cap</a></li>
 		</ul>
 
 	</div>
@@ -131,7 +131,7 @@
 			<?php
 			include ('function/addcart.php');
 
-				$query = $conn->query("SELECT *FROM product WHERE category='basketball' ORDER BY product_id DESC") or die (mysqli_error());
+				$query = $conn->query("SELECT *FROM product WHERE category='top' ORDER BY product_id DESC") or die (mysqli_error());
 
 					while($fetch = $query->fetch_array())
 						{
@@ -147,7 +147,7 @@
 						}else{
 							echo "<div class='float'>";
 							echo "<center>";
-							echo "<a href='details.php?id=".$fetch['product_id']."'><img class='img-polaroid' src='photo/".$fetch['product_image']."' height = '300px' width = '300px'></a>";
+							echo "<a href='details.php?id=".$fetch['product_id']."'><img class='img-polaroid' src='picture/".$fetch['product_image']."' height = '300px' width = '300px'></a>";
 							echo "".$fetch['product_name']."";
 							echo "<br />";
 							echo "P ".$fetch['product_price']."";
